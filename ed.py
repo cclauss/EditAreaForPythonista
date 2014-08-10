ee99eba4e4b24d32cf790bf1ecef57711f2ef807
cclauss <cclauss@bluewin.ch>
2014-08-10 10:54:53 +0200

    Update ed.py for better error messages, etc.
    Really cool app!!

--- ed.py
+++ ed.py
@@ -3,41 +3,34 @@
 #  uses codemirror to provide a capable editor
 #  lightly wrapped in a webview
 #
-import ui,os,uidir
+import json,os,ui,uidir
 
 def edopen(sender):
     '''open the file named in the textbox, load into editor'''
     w=sender.superview['webview1']
     f=sender.superview['filename']
     try:
-        file=open(f.text)
-        w.eval_js('editAreaLoader.setValue("code",{});'.format(escapestr(file.read())))
-        file.close()
+        with open(f.text) as file:
+            fmt = 'editAreaLoader.setValue("code",{});'
+            w.eval_js(fmt.format(json.dumps(file.read())))
     except (IOError):
-        print 'file not found'
+        print 'file "{}" not found'.format(f.text)
 
 def edsave(sender):
     '''save the editor content to file in filename textbox'''
     w=sender.superview['webview1']
     f=sender.superview['filename']
     try:
-        file=open(f.text,'w')
-        file.write(w.eval_js('editAreaLoader.getValue("code");'))
-        file.close()
+        with open(f.text,'w') as file:
+            file.write(w.eval_js('editAreaLoader.getValue("code");'))
     except(IOError):
-        print('could not save')
+        print('could not save file "{}"'.format(f.text))
 
 def edselect(sender):
     f=sender.superview['filename']
     def setter(s):
         f.text=s
     uidir.getFile(setter)
-    
-def escapestr(s):
-    '''escape quotes, and newlines.  probably needs other things escaped too'''
-    #return s.replace('\n','\\n').replace('"','\\"').replace('\'', "\\'")
-    import json
-    return json.dumps(s)
 
 #main script
 
@@ -45,9 +38,8 @@ e=ui.load_view('ed')
 e['filename'].autocapitalization_type=ui.AUTOCAPITALIZE_NONE
 e['filename'].autocorrection_type=False
 #srcname='ed.html'  #doesnt work in webview?.  but does in webbrowser...wth?
-srcname='editarea.html'
+#srcname='editarea.html'
 #print os.path.abspath(srcname)
-w=e['webview1']
-w.load_url(os.path.abspath(srcname))
+e['webview1'].load_url(os.path.abspath('editarea.html'))
 
-e.present('panel') 
+e.present('panel')
